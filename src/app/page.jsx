f1010d6ac26b985f56458a4c68b70c2682bc69b6
Diego Flores Alcantar <diegoedvflores03@gmail.com>
2025-03-23 14:56:17 -0600

    add cover page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,25 +1,34 @@
+'use client'
 import Link from 'next/link';
-import React from 'react';
-import './main.sass'
-
-
-
+import './main.sass';
+import { useEffect } from 'react';
 // Componente principal
 export default function Home() {
+  // Usa useEffect para mostrar la alerta cuando el componente se monta
+  useEffect(() => {
+    const portada = `
+      Universidad Autónoma de Occidente
+      Nombre: Flores Alcantar Diego Edvaldo
+      Matrícula: 23020026
+      Fecha: 24 / 03 / 2025
+    `;
+    window.alert(portada);
+  }, []); // El array vacío asegura que esto solo se ejecute una vez al montar el componente
+
   return (
-      <div className='inicioContain'>
-        <h1>Ejercicios de chi2 Simulación <br /> utilizando JavaScript 🤩</h1>
-        <nav>
-          <Link href="/chocolates">
-            Chocolates 🍫
-          </Link>
-          <Link href="/elecciones">
-            Elecciones 🗳️
-          </Link>
-          <Link href="/menu">
-            Menú 🍽️
-          </Link>
-        </nav>
-      </div>
+    <div className='inicioContain'>
+      <h1>Ejercicios de chi2 Simulación <br /> utilizando JavaScript 🤩</h1>
+      <nav>
+        <Link href="/chocolates">
+          Chocolates 🍫
+        </Link>
+        <Link href="/elecciones">
+          Elecciones 🗳️
+        </Link>
+        <Link href="/menu">
+          Menú 🍽️
+        </Link>
+      </nav>
+    </div>
   );
 }
